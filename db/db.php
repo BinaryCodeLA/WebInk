6cfc318ab0022bb1ee8d26611a7c1396c05f5320
BitFounder <47802830+BitFounder@users.noreply.github.com>
2019-04-01 17:03:53 -0600

    enmascarado credenciales

--- db/db.php
+++ db/db.php
@@ -1,8 +1,8 @@
 <?php
 class Database{
     public static function connect(){
-//        $db = mysqli_connect('localhost','id8523223_controlador','Developer-19..','id8523223_own');
-        $db = mysqli_connect('localhost','id8523223_controlador','Developer-19','id8523223_own');
+//        $db = mysqli_connect('localhost','xxx','xxx','xx');
+        $db = mysqli_connect('localhost','xxx','xxx','xxx');
         $db ->query("SET NAMES 'utf-8'");
         return $db;
     }
